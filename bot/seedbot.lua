4d6704150c7470141b001faa59d76a14cf5a2fd3
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-05-30 13:35:59 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -230,7 +230,7 @@ function create_config( )
     "leave_ban",
 	"supergroup",
 	"whitelist",
-	"msg_checks"
+	"msg_checks",
 	"plugins"
     },
     sudo_users = {223216007,195092846,197026278,210740469,0,tonumber(our_id)},--Sudo users
